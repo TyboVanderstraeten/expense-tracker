e11ac587fcb0f6d8707029fc5b131433a610f9fd
Tybo Vanderstraeten <vanderstraeten.tybo@hotmail.com>
2020-11-07 18:51:24 +0100

    Add statisticsviewmodel

---
+++ ExpenseTracker/ExpenseTracker/ViewModels/StatisticsViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace ExpenseTracker.ViewModels
+{
+    public class StatisticsViewModel : BaseViewModel
+    {
+        #region Private fields
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Constructors
+        public StatisticsViewModel()
+        {
+        }
+        #endregion
+    }
+}
--- ExpenseTracker/ExpenseTracker/Views/StatisticsPage.xaml.cs
+++ ExpenseTracker/ExpenseTracker/Views/StatisticsPage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using ExpenseTracker.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +7,21 @@ namespace ExpenseTracker.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class StatisticsPage : ContentPage
     {
+        #region Private fields
+        private readonly StatisticsViewModel _statisticsViewModel;
+        #endregion
+
+        #region Constructors
         public StatisticsPage()
         {
             InitializeComponent();
+
+            BindingContext = _statisticsViewModel = new StatisticsViewModel();
+
         }
+        #endregion
+
+        #region Methods
+        #endregion
     }
 }
